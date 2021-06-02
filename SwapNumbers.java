28c315e5af799af38b713616ee24081517a444e1
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-02 21:20:44 +0530

    added: swapping of two numbers program

---
+++ SwapNumbers.java
@@ -0,0 +1,25 @@
+package coreprograms;
+
+import java.util.Scanner;
+
+public class SwapNumbers {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the first number: ");
+		int number1 = scanner.nextInt();
+		System.out.println("Enter the second number: ");
+		int number2 = scanner.nextInt();
+		
+		//Before swapping
+		System.out.println("Before swapping numbers are: "+number1+" "+number2);
+		
+		number1 = number1 + number2;
+		number2 = number1 - number2;
+		number1 = number1 - number2;
+		
+		System.out.println("After swapping the numbers are: "+number1+" "+number2);
+
+	}
+
+}
